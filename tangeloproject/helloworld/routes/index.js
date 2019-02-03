912e55f178fcf93b4580e41c6059a4a2071e6f3f
Jaret Espinosa <jaret.espinosa@Jarets-MacBook-Pro.local>
2019-02-02 22:20:13 -0500

    ACM1PT

--- tangeloproject/helloworld/routes/index.js
+++ tangeloproject/helloworld/routes/index.js
@@ -7,13 +7,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-const express = require('express')
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-});
